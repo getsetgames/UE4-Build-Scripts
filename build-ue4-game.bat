a97c3afa59d2ec60ec47865ce7f28e2aec70a3da
Derek van Vliet <derek@getsetgames.com>
2014-10-31 14:17:37 -0400

    old archive directory is removed if it exists

--- build-ue4-game.bat
+++ build-ue4-game.bat
@@ -46,6 +46,11 @@ echo REQUIRED_FILES_2=%REQUIRED_FILES_2%
 echo REQUIRED_FILES_OPT=%REQUIRED_FILES_OPT%
 echo ARCHIVE_DIRECTORY=%ARCHIVE_DIRECTORY%
 
+if exist "%ARCHIVE_DIRECTORY%" (
+	echo Removing old archive directory...
+  rmdir /s /q "%ARCHIVE_DIRECTORY%"
+)
+
 if exist %ENGINE_DIRECTORY% (
 	echo Updating the engine...
 	cd %ENGINE_DIRECTORY%
